1f038857cd0bf31b0c1037e1ebc1db4ddac88086
benny1020 <benny1020@naver.com>
2022-11-14 09:19:07 +0900

    [chore] 프로젝트 환경설정

---
+++ STARTERS_MIDTERM/src/main/java/com/starters/starters_midterm/StartersMidtermApplication.java
@@ -0,0 +1,13 @@
+package com.starters.starters_midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartersMidtermApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartersMidtermApplication.class, args);
+    }
+
+}
---
+++ STARTERS_MIDTERM/src/test/java/com/starters/starters_midterm/StartersMidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starters.starters_midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartersMidtermApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
